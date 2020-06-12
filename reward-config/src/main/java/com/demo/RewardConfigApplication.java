f78878c475b1927b48e5f99799ccc53654d271f6
jh-zhu <Jiahao Zhu@DESKTOP-M6QVNF4.mshome.net>
2020-06-12 12:30:27 -0400

    add config center

---
+++ reward-config/src/main/java/com/demo/RewardConfigApplication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class RewardConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardConfigApplication.class, args);
+	}
+
+}
